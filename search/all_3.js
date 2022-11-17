2a41c3edb9224f800ebd55c4850729ba4388c0c4
joshua-8 <joshua-8@users.noreply.github.com>
2022-11-17 02:11:18 +0000

    deploy: 40e6756c3c620e3408aaab07648dd5a91b7aac6a

--- search/all_3.js
+++ search/all_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['decellimit_0',['decelLimit',['../class_derivs___limiter.html#af3b6f00ed258105dabd57b592add93ca',1,'Derivs_Limiter']]],
-  ['derivs_5flimiter_1',['Derivs_Limiter',['../class_derivs___limiter.html',1,'Derivs_Limiter'],['../class_derivs___limiter.html#a674339d1db4ca3a2358e1d25b9eb6cf4',1,'Derivs_Limiter::Derivs_Limiter()'],['../index.html',1,'(Global Namespace)']]],
+  ['derivs_5flimiter_1',['Derivs_Limiter',['../class_derivs___limiter.html',1,'Derivs_Limiter'],['../class_derivs___limiter.html#a674339d1db4ca3a2358e1d25b9eb6cf4',1,'Derivs_Limiter::Derivs_Limiter(float _velLimit, float _accelLimit, float _decelLimit=NAN, float _target=0, float _startPos=0, float _startVel=0, bool _preventGoingWrongWay=false, bool _preventGoingTooFast=false, float _posLimitLow=-INFINITY, float _posLimitHigh=INFINITY, float _maxStoppingDecel=2, float *_posPointer=NULL, float *_velPointer=NULL)'],['../class_derivs___limiter.html#ab06ebbec7c353a0142e7ab8725ea9b86',1,'Derivs_Limiter::Derivs_Limiter()'],['../index.html',1,'(Global Namespace)']]],
   ['derivs_5flimiter_2eh_2',['Derivs_Limiter.h',['../_derivs___limiter_8h.html',1,'']]],
   ['disttotarget_3',['distToTarget',['../class_derivs___limiter.html#a1c7b224061c2051e9451818d341e3eb4',1,'Derivs_Limiter']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['derivs_5flimiter_0',['Derivs_Limiter',['../class_derivs___limiter.html#a674339d1db4ca3a2358e1d25b9eb6cf4',1,'Derivs_Limiter']]],
+  ['derivs_5flimiter_0',['Derivs_Limiter',['../class_derivs___limiter.html#a674339d1db4ca3a2358e1d25b9eb6cf4',1,'Derivs_Limiter::Derivs_Limiter(float _velLimit, float _accelLimit, float _decelLimit=NAN, float _target=0, float _startPos=0, float _startVel=0, bool _preventGoingWrongWay=false, bool _preventGoingTooFast=false, float _posLimitLow=-INFINITY, float _posLimitHigh=INFINITY, float _maxStoppingDecel=2, float *_posPointer=NULL, float *_velPointer=NULL)'],['../class_derivs___limiter.html#ab06ebbec7c353a0142e7ab8725ea9b86',1,'Derivs_Limiter::Derivs_Limiter()']]],
   ['disttotarget_1',['distToTarget',['../class_derivs___limiter.html#a1c7b224061c2051e9451818d341e3eb4',1,'Derivs_Limiter']]]
 ];
